f80945ac492774d22d9d3e7ac9c2eb7ccb9c6ff7
ArtOfNothing <aguilarericka60@gmail.com>
2022-11-23 18:10:21 -0600

    diseño de interfaces preliminares

--- ProyectoBD1/Clases/Proveedores.Designer.cs
+++ ProyectoBD1/Clases/Proveedores.Designer.cs
@@ -33,39 +33,30 @@
             this.label1 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnActualizar = new System.Windows.Forms.Button();
             this.btnInsertar = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.gpbxPersonal = new System.Windows.Forms.GroupBox();
-            this.txtA = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.txtDocumento1 = new System.Windows.Forms.TextBox();
-            this.txtFirstName = new System.Windows.Forms.TextBox();
-            this.lblFirstName = new System.Windows.Forms.Label();
-            this.lblId = new System.Windows.Forms.Label();
-            this.dgvEmpleado = new System.Windows.Forms.DataGridView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvEmpleado = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            this.gpbxPersonal.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleado)).BeginInit();
+            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -125,7 +116,6 @@
             this.tabPage1.Controls.Add(this.btnActualizar);
             this.tabPage1.Controls.Add(this.btnInsertar);
             this.tabPage1.Controls.Add(this.groupBox1);
-            this.tabPage1.Controls.Add(this.gpbxPersonal);
             this.tabPage1.Controls.Add(this.dgvEmpleado);
             this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -135,24 +125,6 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Gestión Proveedores";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.label7);
-            this.tabPage2.Controls.Add(this.radioButton2);
-            this.tabPage2.Controls.Add(this.radioButton1);
-            this.tabPage2.Controls.Add(this.dataGridView1);
-            this.tabPage2.Controls.Add(this.button1);
-            this.tabPage2.Controls.Add(this.label6);
-            this.tabPage2.Controls.Add(this.textBox3);
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.tabPage2.Size = new System.Drawing.Size(1364, 821);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Ver Proveedores";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // btnEliminar
             // 
@@ -192,6 +164,8 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.textBox2);
@@ -199,109 +173,12 @@
             this.groupBox1.Controls.Add(this.textBox5);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.ForeColor = System.Drawing.Color.DarkSlateBlue;
-            this.groupBox1.Location = new System.Drawing.Point(726, 314);
+            this.groupBox1.Location = new System.Drawing.Point(20, 313);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(586, 191);
+            this.groupBox1.Size = new System.Drawing.Size(1292, 191);
             this.groupBox1.TabIndex = 59;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Proveedores";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(204, 34);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(318, 30);
-            this.textBox5.TabIndex = 27;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(116, 44);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(64, 20);
-            this.label5.TabIndex = 22;
-            this.label5.Text = "Codigo:";
-            // 
-            // gpbxPersonal
-            // 
-            this.gpbxPersonal.Controls.Add(this.txtA);
-            this.gpbxPersonal.Controls.Add(this.label8);
-            this.gpbxPersonal.Controls.Add(this.txtDocumento1);
-            this.gpbxPersonal.Controls.Add(this.txtFirstName);
-            this.gpbxPersonal.Controls.Add(this.lblFirstName);
-            this.gpbxPersonal.Controls.Add(this.lblId);
-            this.gpbxPersonal.ForeColor = System.Drawing.Color.DarkSlateBlue;
-            this.gpbxPersonal.Location = new System.Drawing.Point(20, 314);
-            this.gpbxPersonal.Name = "gpbxPersonal";
-            this.gpbxPersonal.Size = new System.Drawing.Size(700, 191);
-            this.gpbxPersonal.TabIndex = 58;
-            this.gpbxPersonal.TabStop = false;
-            this.gpbxPersonal.Text = "Registro de empresa";
-            // 
-            // txtA
-            // 
-            this.txtA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtA.Location = new System.Drawing.Point(223, 129);
-            this.txtA.Name = "txtA";
-            this.txtA.Size = new System.Drawing.Size(216, 30);
-            this.txtA.TabIndex = 35;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(115, 139);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(90, 20);
-            this.label8.TabIndex = 34;
-            this.label8.Text = "Direccion?:";
-            // 
-            // txtDocumento1
-            // 
-            this.txtDocumento1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDocumento1.Location = new System.Drawing.Point(223, 34);
-            this.txtDocumento1.Name = "txtDocumento1";
-            this.txtDocumento1.Size = new System.Drawing.Size(382, 30);
-            this.txtDocumento1.TabIndex = 31;
-            // 
-            // txtFirstName
-            // 
-            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFirstName.Location = new System.Drawing.Point(223, 84);
-            this.txtFirstName.Name = "txtFirstName";
-            this.txtFirstName.Size = new System.Drawing.Size(382, 30);
-            this.txtFirstName.TabIndex = 27;
-            // 
-            // lblFirstName
-            // 
-            this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFirstName.Location = new System.Drawing.Point(29, 91);
-            this.lblFirstName.Name = "lblFirstName";
-            this.lblFirstName.Size = new System.Drawing.Size(176, 20);
-            this.lblFirstName.TabIndex = 22;
-            this.lblFirstName.Text = "Nombre de la empresa:";
-            // 
-            // lblId
-            // 
-            this.lblId.AutoSize = true;
-            this.lblId.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblId.Location = new System.Drawing.Point(37, 41);
-            this.lblId.Name = "lblId";
-            this.lblId.Size = new System.Drawing.Size(168, 20);
-            this.lblId.TabIndex = 18;
-            this.lblId.Text = "Codigo de la empresa:";
-            // 
-            // dgvEmpleado
-            // 
-            this.dgvEmpleado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvEmpleado.Location = new System.Drawing.Point(20, 18);
-            this.dgvEmpleado.Name = "dgvEmpleado";
-            this.dgvEmpleado.RowTemplate.Height = 28;
-            this.dgvEmpleado.Size = new System.Drawing.Size(1292, 272);
-            this.dgvEmpleado.TabIndex = 57;
             // 
             // textBox1
             // 
@@ -339,10 +216,52 @@
             this.label4.TabIndex = 36;
             this.label4.Text = "Nombre proveedor:";
             // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(204, 34);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(318, 30);
+            this.textBox5.TabIndex = 27;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(116, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Codigo:";
+            // 
+            // dgvEmpleado
+            // 
+            this.dgvEmpleado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmpleado.Location = new System.Drawing.Point(20, 18);
+            this.dgvEmpleado.Name = "dgvEmpleado";
+            this.dgvEmpleado.RowTemplate.Height = 28;
+            this.dgvEmpleado.Size = new System.Drawing.Size(1292, 272);
+            this.dgvEmpleado.TabIndex = 57;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage2.Size = new System.Drawing.Size(1364, 821);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Ver Proveedores";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(38, 275);
+            this.dataGridView1.Location = new System.Drawing.Point(38, 162);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.Size = new System.Drawing.Size(1292, 516);
@@ -353,7 +272,7 @@
             this.button1.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.button1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(413, 199);
+            this.button1.Location = new System.Drawing.Point(413, 86);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(126, 34);
             this.button1.TabIndex = 59;
@@ -364,7 +283,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.ForeColor = System.Drawing.Color.DarkSlateBlue;
-            this.label6.Location = new System.Drawing.Point(34, 156);
+            this.label6.Location = new System.Drawing.Point(34, 43);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(267, 20);
             this.label6.TabIndex = 58;
@@ -372,43 +291,29 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(38, 203);
+            this.textBox3.Location = new System.Drawing.Point(38, 90);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(338, 34);
             this.textBox3.TabIndex = 57;
             // 
-            // radioButton1
+            // textBox4
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(38, 97);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(110, 24);
-            this.radioButton1.TabIndex = 61;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Proveedor";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(742, 37);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(318, 30);
+            this.textBox4.TabIndex = 41;
             // 
-            // radioButton2
+            // label8
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(191, 97);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(101, 24);
-            this.radioButton2.TabIndex = 62;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Empresa";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.ForeColor = System.Drawing.Color.DarkSlateBlue;
-            this.label7.Location = new System.Drawing.Point(34, 45);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(281, 20);
-            this.label7.TabIndex = 63;
-            this.label7.Text = "Indique el registro que desea buscar";
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(618, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 20);
+            this.label8.TabIndex = 40;
+            this.label8.Text = "Dirección:";
             // 
             // Proveedores
             // 
@@ -426,13 +331,11 @@
             this.panel1.PerformLayout();
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
-            this.tabPage2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.gpbxPersonal.ResumeLayout(false);
-            this.gpbxPersonal.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleado)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -456,20 +359,12 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.GroupBox gpbxPersonal;
-        private System.Windows.Forms.TextBox txtA;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox txtDocumento1;
-        private System.Windows.Forms.TextBox txtFirstName;
-        private System.Windows.Forms.Label lblFirstName;
-        private System.Windows.Forms.Label lblId;
         private System.Windows.Forms.DataGridView dgvEmpleado;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
     }
 }
